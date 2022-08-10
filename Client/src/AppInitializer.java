72942afdefefa2a0b105c803c125323aa8b4856b
oshadaera68 <mindartlk010@gmail.com>
2022-08-10 22:47:32 +0530

    updated

---
+++ Client/src/AppInitializer.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/LogInForm.fxml"))));
+        primaryStage.setTitle("Play Tech Chat App v0.1 - Log In");
+        primaryStage.show();
+    }
+}
---
+++ Client/src/controller/LogInFormController.java
@@ -0,0 +1,18 @@
+/**
+ * @Owner - Oshada Eranga
+ * @version - v0.1
+ */
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LogInFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+}
